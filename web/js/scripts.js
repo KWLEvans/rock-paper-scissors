9b53cd1915d668b5d89a753bccc41eea3e963979
Keith Evans and Patrick McGreevy <keith@>
2017-02-15 11:06:10 -0800

    Change game to involve keypresses

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -5,25 +5,60 @@ $(function() {
   var player2Played = false;
 
 
-  $("#player-1 button").click(function() {
-    player1Input = $(this).val();
-    player1Played = true;
-    $("#player-1 h3").addClass('played');
-    submitGame();
-  });
+  $("html").keypress(function(event) {
+    $("#result").removeClass("bg-success");
+    $("#result").html("");
+    $("p.choice").hide();
 
-  $("#player-2 button").click(function() {
-    player2Input = $(this).val();
-    player2Played = true;
-    $("#player-2 h3").addClass('played');
+    if (event.which === 65 || event.which === 97) {
+      player1Input = "rock";
+      player1Played = true;
+      $("#player-1-played .ready-tag").show();
+    } else if (event.which === 83 || event.which === 115) {
+      player1Input = "paper";
+      player1Played = true;
+      $("#player-1-played .ready-tag").show();
+    } else if (event.which === 68 || event.which === 100) {
+      player1Input = "scissors";
+      player1Played = true;
+      $("#player-1-played .ready-tag").show();
+    }
+    if (event.which === 74 || event.which === 106) {
+      player2Input = "rock";
+      player2Played = true;
+      $("#player-2-played .ready-tag").show();
+    } else if (event.which === 75 || event.which === 107) {
+      player2Input = "paper";
+      player2Played = true;
+      $("#player-2-played .ready-tag").show();
+    } else if (event.which === 76 || event.which === 108) {
+      player2Input = "scissors";
+      player2Played = true;
+      $("#player-2-played .ready-tag").show();
+    }
     submitGame();
-  });
+  })
 
   function submitGame() {
     if (player1Played && player2Played) {
       $.post("/play", {player1Input: player1Input, player2Input: player2Input}, function(response) {
         $("#result").html(response);
+        $("#result").addClass("bg-success");
+        $(".ready-tag").hide();
+        revealChoices();
+        player1Played = false;
+        player2Played = false;
       });
     }
   }
+
+  function revealChoices() {
+    $("#player-1-played p.choice").html(capitalize(player1Input));
+    $("#player-2-played p.choice").html(capitalize(player2Input));
+    $("p.choice").show();
+  }
 });
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
