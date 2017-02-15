d7da643d97ca276428b138533a18b2e6b09c61bb
Keith Evans and Patrick McGreevy <patrick@>
2017-02-15 09:18:50 -0800

    Write test for tie game

---
+++ src/RockPaperScissors.php
@@ -0,0 +1,9 @@
+<?php
+class RockPaperScissors
+{
+  function playGame($first_player, $second_player)
+  {
+    if ($first_player == $second_player)
+      return 'Tie';
+  }
+}
---
+++ tests/RockPaperScissorsTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'src/RockPaperScissors.php';
+
+class RockPaperScissorsTest extends PHPUnit_Framework_TestCase
+{
+  function test_playGame_tie()
+  {
+    //Arrange
+    $test_RockPaperScissors = new RockPaperScissors;
+    $first_input = 'rock';
+    $second_input = 'rock';
+
+    //Act
+    $result = $test_RockPaperScissors->playGame($first_input, $second_input);
+
+    //Assert
+    $this->assertEquals('Tie', $result);
+  }
+}
