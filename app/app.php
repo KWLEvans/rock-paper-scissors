9edc66497a8e40439a07288ca2292ce3ff849f7d
Keith Evans and Patrick McGreevy <patrick@>
2017-02-15 10:04:08 -0800

    Add basic interface

--- app/app.php
+++ app/app.php
@@ -2,14 +2,26 @@
 date_default_timezone_set('America/Los_Angeles');
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/RockPaperScissors.php';
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), [
   'twig.path' => __DIR__ . '/../views/'
 ]);
 
-$app->get('/', function() {
-  return 'Placeholder root view';
+$app->get('/', function() use ($app) {
+  return $app['twig']->render('play.html.twig');
+});
+
+$app->post('/play', function() use ($app) {
+  $first_input = $_POST['player-1'];
+  $second_input = $_POST['player-2'];
+  $game = new RockPaperScissors;
+  $result = $game->playGame($first_input, $second_input);
+
+  return $app['twig']->render('result.html.twig', [
+    'result' => $result
+  ]);
 });
 
 return $app;
